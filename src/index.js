2e00b6e3c4ebfbf4f29c58f07ef58531cc9bd59f
hmad213 <142343785+hmad213@users.noreply.github.com>
2025-03-29 17:50:08 +0500

    Add dynamic home page and projects

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import "./styles.css"
 import Project from "./js/project"
-import Todo from "./js/project"
+import Todo from "./js/todo"
+import DOM from "./js/UI"
+
 
 const projectList = [];
 projectList.push(new Project("None"));
+projectList.push(new Project("hey"));
+
+projectList[0].addTodo(new Todo("Hello", "abc", 0, true, true))
+
+DOM.initialize(projectList);
 
 export default projectList;
--- src/js/UI.js
+++ src/js/UI.js
@@ -1,15 +1,104 @@
-
+import trash from "../images/trash.svg"
+import starFilled from "../images/star-filled.svg"
+import star from "../images/star.svg"
 
 class DOM{
-    constructor(list){
-        loadHome(list);
+    constructor(){
+        
+    }
+
+    initialize(projectList){
+        this.loadHome(projectList);
+        this.loadProjects(projectList);
     }
 
     loadHome(projectList){
+        let home = document.createElement("div");
+        home.classList.add("home");
 
+        let heading = document.createElement("h1");
+        heading.textContent = "Home";
+        home.appendChild(heading)
+
+        let todoContainer = document.createElement("div");
+        todoContainer.classList.add("todo-container");
+
+        for(let i = 0; i < projectList.length; i++){
+            for(let j = 0; j < projectList[i].todoList.length; j++){
+                todoContainer.appendChild(this.loadTodo(projectList[i].todoList[j]));
+            }
+        }
+        home.appendChild(todoContainer);
+
+        document.querySelector("#content").appendChild(home);
     }
 
-    loadProject(){
+    loadProjects(projectList){
+        let projectContainer = document.querySelector(".project-container");
+        for(let i = 1; i < projectList.length; i++){
+            let div = document.createElement("div");
+            div.classList.add("project");
+
+            let title = document.createElement("span");
+            title.textContent = projectList[i].title;
+
+            let img = document.createElement("img");
+            img.src = trash;
+
+            div.appendChild(title);
+            div.appendChild(img);
+
+            projectContainer.appendChild(div);
+        }
+    }
+
+    loadTodo(todo){
+        let todoDiv = document.createElement("div");
+        todoDiv.classList.add("todo")
+
+        let title = document.createElement("span");
+        title.textContent = todo.title;
+        if(todo.checked){
+            title.style.textDecoration = "line-through";
+        }
+
+        let checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        console.log(todo);
+        console.log(todo.checked);
+        checkbox.checked = todo.checked;
+
+        todoDiv.appendChild(checkbox);
+        todoDiv.appendChild(title);
+
+        let starImg = document.createElement("img")
+        if(todo.priority){
+            starImg.src = starFilled;
+        }else{
+            starImg.src = star;
+        }
+        todoDiv.appendChild(starImg);
         
+        let trashImg = document.createElement("img");
+        trashImg.src = trash;
+        todoDiv.appendChild(trashImg)
+        
+        return todoDiv;
+    }
+
+    clearContent(){
+        document.querySelector("#content").innerHTML = "";
+    }
+
+    clearProjects(){
+        document.querySelector(".project-container").innerHTML = "";
+    }
+
+    addProject(){
+        return;
     }
 }
+
+let d = new DOM;
+
+export default d;
